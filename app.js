d02325bd9ce55fff13cd6ea44c52153be8bdb5b1
THARUSHA <142484084+Tharushz@users.noreply.github.com>
2024-08-15 16:27:13 +0530

    frist commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const lowercaseLetters ="abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers ="0123456789";
+const symbols ="!@#$%^&*()_-+=/?<>";
+
+
+const lengthEl =document.getElementById("length");
+const lowercaseEl = document.getElementById('lowercase');
+const uppercaseEl = document.getElementById('uppercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generatebtn = document.getElementById('generate');
+const passwordEl = document.getElementById('password');
+
+
+generatebtn.addEventListener('click',function(){
+
+    const length =lengthEl.value;
+    let characters = "";
+    let password = "";
+
+    
+
+    if (lowercaseEl.checked){
+        characters += lowercaseLetters;
+    }
+
+    if (uppercaseEl.checked){
+        characters += uppercaseLetters;
+    }
+
+    if (numbersEl.checked){
+        characters += numbers;
+    }
+
+    if (symbolsEl.checked){
+        characters += symbols;
+    }
+
+
+
+    for (let i=0; i < length; i++){
+
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+
+    passwordEl.value = password ;
+    
+
+});
